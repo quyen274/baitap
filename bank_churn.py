3e5df348baf5c645f8e569a1350baecc7f0253b2
quyen274 <162405422+quyen274@users.noreply.github.com>
2024-05-26 22:53:05 +0700

    Update and rename Untitled8.ipynb to bank_churn.py

---
+++ bank_churn.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import pickle as pickle
+import streamlit as st
+d= pd.read_csv('train.csv')
+feature=['id','Age','CreditScore','Balance','EstimatedSalary']
+target=['Exited']
+X=d[feature]
+y=d[target]
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size= 1/4, random_state=42,)
+clf = LogisticRegression()
+clf.fit(X_train, y_train)
+
+print(clf.score(X_test, y_test))
+
+print("Saving model to pickle file.")
+pickle.dump(clf, open("bank_churn_1.pkl", 'wb'))
