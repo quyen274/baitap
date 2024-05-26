13ef08a0d7e979777cf79db7219d2bca002e6da0
quyen274 <162405422+quyen274@users.noreply.github.com>
2024-05-26 23:12:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pickle
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()
 clf=pickle.load(open('bank_churn_1.pkl','rb'))
 st.title("Dự đoán sự rời bỏ của khách hàng ngân hàng")
 id=st.number_input("id khách",0,82516,1002)
@@ -15,7 +13,6 @@ prediction = clf.predict([[id,Age,CreditScore,Balance,EstimatedSalary]])
 d=pd.read_csv('train.csv')
 if st.button("Dự đoán"):
     input_data = np.array([[id, Age, CreditScore, Balance, EstimatedSalary]])
-    input_data_scaled = scaler.transform(input_data)
     prediction = model.predict(input_data_scaled)
     predicted_class = "Rời bỏ" if prediction[0] == 1 else "Ở lại"
     st.write(f"Dự đoán: {predicted_class}")
