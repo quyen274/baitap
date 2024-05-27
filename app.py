36e62c64886cf043f9155a22c806921c49c586f1
quyen274 <162405422+quyen274@users.noreply.github.com>
2024-05-27 22:41:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 clf=pickle.load(open('bank_churn_1.pkl','rb'))
 st.title("Dự đoán sự rời bỏ của khách hàng ngân hàng")
 id=st.number_input("id khách",0,82516,1002)
-Age = st.number_input("Tuổi", 18, 38, 30)
+Age = st.number_input("Tuổi", 18, 42, 30)
 CreditScore = st.number_input("Điểm tín dụng", 0, 656, 600)
 Balance = st.number_input("Số dư tài khoản", 0.0, 55478.086689, 50000.0)
 EstimatedSalary = st.number_input("Lương ước tính", 0.0, 112574.822734, 53213.0)
